cf58df27d5eb896b01d32a2441d437e23c04c62f
0xpsyduck <0xpsyduck@gmail.com>
2024-08-31 22:13:36 +0530

    new

---
+++ frontend/src/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './styles.css';
+const Footer = () => (
+  <footer className="footer bg-white text-dark py-3 mt-auto">
+    <div className="container" style={{color: 'black'}}>
+      <span className="text-dark "><h5>Developed by Casper FFG. Check out the code on <a  href="https://github.com/abhinavjain1110/WebTracker" target="_blank" rel="noopener noreferrer" className="text-dark" style={{fontWeight: 'bold'}}>GitHub</a>.</h5></span>
+    </div>
+  </footer>
+);
+
+export default Footer;
---
+++ frontend/src/index.js
@@ -0,0 +1,31 @@
+/* import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
+ */
+// index.js
+// index.js
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router } from 'react-router-dom';
+import App from './App';
+import './index.css';
+import Footer from './Footer';
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <Router>
+    <App />
+    <Footer/>
+  </Router>
+);
+
+
