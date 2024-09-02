07629292a97bb651a032e9999860ced742a13641
psyduck <0xpsyduck@gmail.com>
2024-09-02 22:01:15 +0530

    added upcoming features

--- frontend/src/components/UpcomingFeatures.js
+++ frontend/src/components/UpcomingFeatures.js
@@ -7,11 +7,13 @@ const UpcomingFeatures = () => {
       <h2>Upcoming Features</h2>
       <ul>
         <li>Integrate more chains like BTC, Monero, ZCash, Solana and many more in addition to ETH.</li>
-        <li>Integrate a custom trained Machine Learning model like Ollama to automate the process of finding the end reciever by parsing through various transaction paths and ease the process for our product's end users.</li>
-        <li>Use Bitquery's GraphQL APIs for a pulling data about the chain which is more customized and faster than other APIs.</li>
-	<li>Give more options to users in monitoring section to get notification via browser push, E-Mail and Slack, already implemented Telegram notifications.</li>
-	<li>Improve UI/UX of the website.</li>
-	<li>Collaborate and take feedback from users to improve our project.</li>
+        <li>Integrate a custom trained <b>Machine Learning</b> model like <b>Ollama</b> to automate the process of finding the end reciever by parsing through various transaction paths and ease the process for our app's end users.</li>
+        <li>Use <b>Bitquery's GraphQL APIs</b> for a pulling data about the chain which is more customized and faster than other APIs.</li>
+      	<li>Give more options to users in monitoring section to get notification via browser push, E-Mail and Slack, already implemented <b>Telegram notifications</b>.</li>
+	      <li>Improve UI/UX of the website.</li>
+	      <li>Collaborate and take feedback from users to further improve our webapp.</li>
+        <li>Introduce more <b>Github Actions CI/CD piplelines</b> for automating boilerplate work.</li>
+        <li>and much more !!!</li>
       </ul>
     </div>
   );
