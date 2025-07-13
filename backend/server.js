1ac2815b3755eb66f6baef8ea7788f7e25dda260
Abhinav Jain <abhinavjain11102004@gmail.com>
2025-07-13 16:01:06 +0530

    Cors

--- backend/server.js
+++ backend/server.js
@@ -48,7 +48,9 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 
-app.use(cors());
+app.use(cors({
+  origin: 'https://crypto-tracer-two.vercel.app'
+}));
 app.use(express.json());
 
 app.get('/', (req, res) => res.send('CryptoTracer'));
